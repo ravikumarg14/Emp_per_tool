1e1b559d5ef462c34c49c67aff88fb78f799217e
ravikumarg14 <120389974+ravikumarg14@users.noreply.github.com>
2023-08-30 16:45:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ with st.form("entry_form", clear_on_submit=True):
     if submitted:
         db.insert_period(teamname, reviwername, doctype, number,rev,pages,description,startdate,enddate)
         st.write(type(db.fetch_all_periods()))
-        df=pd.read_json(db.fetch_all_periods())
+        #df=pd.read_json(db.fetch_all_periods())
         st.write(db.fetch_all_periods())
         st.success("Data saved!")
 
